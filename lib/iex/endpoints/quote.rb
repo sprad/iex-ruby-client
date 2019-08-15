a0eb463c6807e9bd1e34d018793be0f5fcce10cc
Justin Spradlin <jspradlin@gmail.com>
2019-08-14 20:50:40 -0400

    Adding Batch for Quotes

--- lib/iex/endpoints/quote.rb
+++ lib/iex/endpoints/quote.rb
@@ -6,6 +6,18 @@ module IEX
       rescue Faraday::ResourceNotFound => e
         raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
       end
+
+      def quote_batch(symbols = [])
+        options = {
+          "symbols" => symbols.join(","),
+          "types" => "quote"
+        }
+        get("stock/market/batch", options).map do |data|
+          IEX::Resources::Quote.new(data.last["quote"])
+        end
+      rescue Faraday::ResourceNotFound => e
+        raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
+      end
     end
   end
 end
